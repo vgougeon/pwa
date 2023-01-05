6d1659955b7876230221f1e57af581430ff28c6d
Vincent GOUGEON <gougeonvincent31@gmail.com>
2023-01-05 11:09:03 +0100

    test

--- serviceworker.js
+++ serviceworker.js
@@ -1,31 +1,43 @@
+const urlsToCache = [
+    "/",
+    "/pwa/index.js",
+    "/pwa/styles.css",
+    "/pwa/index.html",
+    "/pwa/icon.png",
+    "/pwa/icon120.png",
+    "/pwa/icon.svg"
+];
 self.addEventListener("install", event => {
-    console.log("Service worker installed");
+    event.waitUntil(
+        caches.open("pwa-assets")
+            .then(cache => {
+                cache.addAll(urlsToCache);
+            })
+    );
 });
+
 self.addEventListener("activate", event => {
     console.log("Service worker activated");
 });
 
 self.addEventListener("fetch", (event) => {
     console.log(`Handling fetch event for ${event.request.url}`);
-  
+
     event.respondWith(
-      caches.match(event.request).then((response) => {
-        if (response) {
-          console.log("Found response in cache:", response);
-          return response;
-        }
-        console.log("No response found in cache. About to fetch from network…");
-  
-        return fetch(event.request)
-          .then((response) => {
-            console.log("Response from network is:", response);
-  
-            return response;
-          })
-          .catch((error) => {
-            console.error(`Fetching failed: ${error}`);
-            throw error;
-          });
-      })
+        caches.match(event.request).then((response) => {
+            if (response) {
+                console.log("Found response in cache:", response);
+                return response;
+            }
+            console.log("No response found in cache. About to fetch from network…");
+
+            return fetch(event.request)
+                .then(async (response) => {
+                    console.log("Response from network is:", response);
+                    const cache = await caches.open("pwa-assets");
+                    cache.put(e.request, response.clone());
+                    return response;
+                })
+        })
     );
-  });
+});
