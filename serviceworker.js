271a962c13f821cb1f7e8d7a29d4cc5dfb831340
Vincent GOUGEON <gougeonvincent31@gmail.com>
2023-01-05 09:56:04 +0100

    fetch

--- serviceworker.js
+++ serviceworker.js
@@ -4,3 +4,28 @@ self.addEventListener("install", event => {
 self.addEventListener("activate", event => {
     console.log("Service worker activated");
 });
+
+self.addEventListener("fetch", (event) => {
+    console.log(`Handling fetch event for ${event.request.url}`);
+  
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        if (response) {
+          console.log("Found response in cache:", response);
+          return response;
+        }
+        console.log("No response found in cache. About to fetch from network…");
+  
+        return fetch(event.request)
+          .then((response) => {
+            console.log("Response from network is:", response);
+  
+            return response;
+          })
+          .catch((error) => {
+            console.error(`Fetching failed: ${error}`);
+            throw error;
+          });
+      })
+    );
+  });
