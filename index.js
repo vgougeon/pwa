a1a141d9f7a3cec5d31f49e8575c47708e36107a
Vincent GOUGEON <gougeonvincent31@gmail.com>
2023-01-05 09:41:17 +0100

    ok

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register("/serviceworker.js");
+    navigator.serviceWorker.register("https://vgougeon.github.io/pwa/serviceworker.js");
 }
